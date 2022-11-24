503115a9ae3eaeaa46bb72b165d176a82d0b9585
slimani44u <slimani44u@etu.univ-lorraine.fr>
2022-11-24 09:59:01 +0100

    base de donnees

---
+++ baseDeDonnees.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <h1>Boisson</h1>
+		<meta charset="UTF-8">
+    </head>
+    <body>
+      <?php
+    
+    $mysqli = new mysqli("127.0.0.1","user","mdp","base");
+
+      ?>
+    </body>
+</html>
