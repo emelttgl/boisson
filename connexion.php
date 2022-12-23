c8ac9d976362d66d71637da7220990c3e20e72ce
slimani44u <slimani44u@etu.univ-lorraine.fr>
2022-12-23 14:37:45 +0100

    connexion fini et inscription a tester

--- connexion.php
+++ connexion.php
@@ -8,7 +8,7 @@
     <div id="container">
       <!-- zone de connexion -->
  
-      <form action="verification.php" method="POST">
+      <form action="" method="POST">
         <h1>Connexion</h1>
  
         <label><b>Nom d'utilisateur</b></label>
@@ -17,13 +17,37 @@
         <label><b>Mot de passe</b></label>
         <input type="password" placeholder="Entrer le mot de passe" name="motDePasse" required>
 
-        <input type="submit" id='submit' value='CONNEXION' >
+        <input type="submit" id='submit' name ='connexion' value='CONNEXION' >
         <?php
-          if(isset($_GET['erreur'])){
-            $err = $_GET['erreur'];
-            if($err==1 || $err==2)
-                echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-          }
+
+          session_start();
+            if(isset($_POST['connexion'])){
+              try{
+                $id=$_POST['id'];  
+                $motDePasse=$_POST['motDePasse']; 
+                $bdd = new PDO('mysql:host=localhost;dbname=boisson;charset=utf8', 'root', 'root');
+                $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $req = $bdd->query("SELECT COUNT(*) as Nb FROM UTILISATEUR WHERE id = '$id' AND motDePasse = '$motDePasse' ;");
+                $req->execute();
+                $result= $req->fetch();
+                $count = $result['Nb'];
+                if($count!=0){ // nom d'utilisateur et mot de passe correctes
+        
+                  $_SESSION['id'] = $id;
+                  header('Location: principale.php');
+                }
+                else{
+                  
+                  echo "<p style='color:pink'>Utilisateur ou mot de passe incorrect</p>";
+                }
+              }
+              catch(Exception $e){
+                  die('nom d utilisateur est une cle primaire');
+              }
+         
+            }
+        
         ?>
       </form>
    </div>
